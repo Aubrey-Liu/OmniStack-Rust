99a3201e786dd0a80e52218eca9addd0d4d70e11
JeremyGuo <jeremyguo@pku.edu.cn>
2023-06-13 14:22:32 +0800

    Defined all symbols need to rewrite

--- src/user_library/symb.h
+++ src/user_library/symb.h
@@ -9,6 +9,15 @@
 #include <dlfcn.h>
 #include <stdexcept>
 #include <pthread.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/time.h>
+
+#if defined(__APPLE__)
+#include <sys/event.h>
+#elif defined(__linux__)
+#include <sys/epoll.h>
+#endif
 
 #ifndef omnistack_typeof
 #ifndef typeof
@@ -27,11 +36,33 @@ namespace omnistack {
         namespace posix_api {
             extern omnistack_typeof(::fork)* fork;
             extern omnistack_typeof(::pthread_create)* pthread_create;
+            extern omnistack_typeof(::socket) socket;
+            extern omnistack_typeof(::close) close;
+
+            extern omnistack_typeof(::read) read;
+            extern omnistack_typeof(::recv) recv;
+            extern omnistack_typeof(::recvfrom) recvfrom;
+
+            extern omnistack_typeof(::write) write;
+            extern omnistack_typeof(::send) send;
+            extern omnistack_typeof(::sendto) sendto;
+
+#if defined(__APPLE__)
+            extern omnistack_typeof(::kqueue) kqueue;
+            extern omnistack_typeof(::kevent) kevent;
+            extern omnistack_typeof(::kevent64) kevent64;
+#elif defined(__linux__)
+            extern omnistack_typeof(::epoll_create) epoll_create;
+            extern omnistack_typeof(::epoll_wait) epoll_wait;
+            extern omnistack_typeof(::epoll_ctl) epoll_ctl;
+#endif
         }
 
         namespace api {
-            pid_t fork();
-
+            omnistack_typeof(::fork) fork;
+            omnistack_typeof(::pthread_create) pthread_create;
+            omnistack_typeof(::socket) socket;
+            omnistack_typeof(::close) close;
         }
     }
 }
