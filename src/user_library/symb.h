aa04862519f7715a6ec5adf8c4a8f1e962dac27a
JeremyGuo <jeremyguo@pku.edu.cn>
2023-06-13 14:29:49 +0800

    Update userlib

--- src/user_library/symb.h
+++ src/user_library/symb.h
@@ -36,25 +36,25 @@ namespace omnistack {
         namespace posix_api {
             extern omnistack_typeof(::fork)* fork;
             extern omnistack_typeof(::pthread_create)* pthread_create;
-            extern omnistack_typeof(::socket) socket;
-            extern omnistack_typeof(::close) close;
+            extern omnistack_typeof(::socket)* socket;
+            extern omnistack_typeof(::close)* close;
 
-            extern omnistack_typeof(::read) read;
-            extern omnistack_typeof(::recv) recv;
-            extern omnistack_typeof(::recvfrom) recvfrom;
+            extern omnistack_typeof(::read)* read;
+            extern omnistack_typeof(::recv)* recv;
+            extern omnistack_typeof(::recvfrom)* recvfrom;
 
-            extern omnistack_typeof(::write) write;
-            extern omnistack_typeof(::send) send;
-            extern omnistack_typeof(::sendto) sendto;
+            extern omnistack_typeof(::write)* write;
+            extern omnistack_typeof(::send)* send;
+            extern omnistack_typeof(::sendto)* sendto;
 
 #if defined(__APPLE__)
-            extern omnistack_typeof(::kqueue) kqueue;
-            extern omnistack_typeof(::kevent) kevent;
-            extern omnistack_typeof(::kevent64) kevent64;
+            extern omnistack_typeof(::kqueue)* kqueue;
+            extern omnistack_typeof(::kevent)* kevent;
+            extern omnistack_typeof(::kevent64)* kevent64;
 #elif defined(__linux__)
-            extern omnistack_typeof(::epoll_create) epoll_create;
-            extern omnistack_typeof(::epoll_wait) epoll_wait;
-            extern omnistack_typeof(::epoll_ctl) epoll_ctl;
+            extern omnistack_typeof(::epoll_create)* epoll_create;
+            extern omnistack_typeof(::epoll_wait)* epoll_wait;
+            extern omnistack_typeof(::epoll_ctl)* epoll_ctl;
 #endif
         }
 
--- src/user_library/symb_origin.cpp
+++ src/user_library/symb_origin.cpp
@@ -9,15 +9,59 @@ namespace omnistack {
         namespace posix_api {
             omnistack_typeof(::fork)* fork = nullptr;
             omnistack_typeof(::pthread_create)* pthread_create = nullptr;
+            omnistack_typeof(::socket)* socket = nullptr;
+            omnistack_typeof(::close)* close = nullptr;
+
+            omnistack_typeof(::read)* read = nullptr;
+            omnistack_typeof(::recv)* recv = nullptr;
+            omnistack_typeof(::recvfrom)* recvfrom = nullptr;
+
+            omnistack_typeof(::write)* write = nullptr;
+            omnistack_typeof(::send)* send = nullptr;
+            omnistack_typeof(::sendto)* sendto = nullptr;
+
+#if defined(__APPLE__)
+            omnistack_typeof(::kqueue)* kqueue = nullptr;
+            omnistack_typeof(::kevent)* kevent = nullptr;
+            omnistack_typeof(::kevent64)* kevent64 = nullptr;
+#elif defined(__linux__)
+            omnistack_typeof(::epoll_create)* epoll_create = nullptr;
+            omnistack_typeof(::epoll_wait)* epoll_wait = nullptr;
+            omnistack_typeof(::epoll_ctl)* epoll_ctl = nullptr;
+#endif
 
             void InitAPI() {
-                posix_api::fork = (omnistack_typeof(::fork)*) dlsym(RTLD_NEXT, "fork");
-                if (!posix_api::fork)
-                    throw std::runtime_error("fork not found in RTLD_NEXT");
+#define INIT_API(api) \
+                do {      \
+                    posix_api::api = (omnistack_typeof(::api)*) dlsym(RTLD_NEXT, #api); \
+                    if (!posix_api::api) \
+                        throw std::runtime_error(#api " not found in RTLD_NEXT");       \
+                } while(0)
 
-                posix_api::pthread_create = (omnistack_typeof(::pthread_create)*) dlsym(RTLD_NEXT, "pthread_create");
-                if (!posix_api::fork)
-                    throw std::runtime_error("pthread create not found in RTLD_NEXT");
+                INIT_API(fork);
+                INIT_API(pthread_create);
+                INIT_API(socket);
+                INIT_API(close);
+
+                INIT_API(read);
+                INIT_API(recv);
+                INIT_API(recvfrom);
+
+                INIT_API(write);
+                INIT_API(send);
+                INIT_API(sendto);
+
+#if defined(__APPLE__)
+                INIT_API(kqueue);
+                INIT_API(kevent);
+                INIT_API(kevent64);
+#elif defined(__linux__)
+                INIT_API(epoll_create);
+                INIT_API(epoll_wait);
+                INIT_API(epoll_ctl);
+#endif
+
+#undef INIT_API
             }
         }
     }
