7cd53e575228e94c45574507c5ff276e0168a0ef
6th <6th@pku.edu.cn>
2023-08-02 10:59:40 +0800

    module fix

---
+++ src/data_plane/modules/ipv4_parser/ipv4_parser.cpp
@@ -0,0 +1,13 @@
+//
+// Created by liuhao on 23-8-1.
+//
+
+#include <omnistack/module/module.hpp>
+
+namespace omnistack::data_plane {
+
+class Ipv4Parser : public Module<Ipv4Parser, std::to_array("Ipv4Parser")> {
+
+};
+
+}
