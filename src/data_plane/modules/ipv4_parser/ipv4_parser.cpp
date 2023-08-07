9ae3771a0cc3127fefa29b084b14aa61ce64940f
6th <6th@pku.edu.cn>
2023-08-07 21:11:47 +0800

    module fix & add sample module

--- src/data_plane/modules/ipv4_parser/ipv4_parser.cpp
+++ src/data_plane/modules/ipv4_parser/ipv4_parser.cpp
@@ -9,37 +9,39 @@
 
 namespace omnistack::data_plane::ipv4_parser {
 
-using namespace omnistack::common;
+    using namespace omnistack::common;
 
-inline constexpr char kName[] = "Ipv4Parser";
+    inline constexpr char kName[] = "Ipv4Parser";
 
-class Ipv4Parser : public Module<Ipv4Parser, kName> {
-public:
-    static bool DefaultFilter(DataPlanePacket* packet);
+    class Ipv4Parser : public Module<Ipv4Parser, kName> {
+    public:
+        Ipv4Parser() {}
 
-    Filter GetFilter(std::string_view upstream_node, uint32_t global_id) override { return DefaultFilter; };
+        static bool DefaultFilter(DataPlanePacket* packet);
 
-    DataPlanePacket* MainLogic(DataPlanePacket* packet) override;
+        Filter GetFilter(std::string_view upstream_node, uint32_t global_id) override { return DefaultFilter; };
 
-    constexpr bool allow_duplication_() override { return true; }
+        DataPlanePacket* MainLogic(DataPlanePacket* packet) override;
 
-    constexpr ModuleType type_() override { return ModuleType::kReadWrite; }
-};
+        constexpr bool allow_duplication_() override { return true; }
 
-bool Ipv4Parser::DefaultFilter(DataPlanePacket* packet) {
-    PacketHeader &eth = packet->packet_headers_[0];
-    EthernetHeader* eth_header = reinterpret_cast<EthernetHeader*>(eth.data_);
-    return eth_header->type == ETH_PROTO_TYPE_IPV4;
-}
+        constexpr ModuleType type_() override { return ModuleType::kReadWrite; }
+    };
 
-DataPlanePacket* Ipv4Parser::MainLogic(DataPlanePacket* packet) {
-    Ipv4Header* ipv4_header = reinterpret_cast<Ipv4Header*>(packet->data_ + packet->offset_);
-    PacketHeader &ipv4 = *(packet->header_tail_ ++);
-    ipv4.length_ = ipv4_header->ihl << 2;
-    ipv4.data_ = reinterpret_cast<unsigned char*>(ipv4_header);
-    packet->length_ = ntohs(ipv4_header->len) + packet->offset_;
-    packet->offset_ += ipv4.length_;
-    return packet;
-}
+    bool Ipv4Parser::DefaultFilter(DataPlanePacket* packet) {
+        PacketHeader &eth = packet->packet_headers_[0];
+        EthernetHeader* eth_header = reinterpret_cast<EthernetHeader*>(eth.data_);
+        return eth_header->type == ETH_PROTO_TYPE_IPV4;
+    }
+
+    DataPlanePacket* Ipv4Parser::MainLogic(DataPlanePacket* packet) {
+        Ipv4Header* ipv4_header = reinterpret_cast<Ipv4Header*>(packet->data_ + packet->offset_);
+        PacketHeader &ipv4 = *(packet->header_tail_ ++);
+        ipv4.length_ = ipv4_header->ihl << 2;
+        ipv4.data_ = reinterpret_cast<unsigned char*>(ipv4_header);
+        packet->length_ = ntohs(ipv4_header->len) + packet->offset_;
+        packet->offset_ += ipv4.length_;
+        return packet;
+    }
 
 }
