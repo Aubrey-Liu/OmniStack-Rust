0e57b7595605d09dc2db363352cffe51add95fd7
22040106-曾启桓 <2697541206@qq.com>
2023-08-24 20:14:39 +0800

    udp_recverer test finished

--- tests/data_plane/modules/udp_recver/udp_recver.cpp
+++ tests/data_plane/modules/udp_recver/udp_recver.cpp
@@ -10,42 +10,55 @@
 #include <omnistack/common/protocol_headers.hpp>
 
 
-TEST(DataPlaneIpv4Sender, Load) {
+TEST(DataPlaneUdpRecver, Load) {
     using namespace omnistack::data_plane;
-    auto handle = dlopen("../lib/libomni_data_plane_ipv4_sender.so", RTLD_NOW | RTLD_GLOBAL);
+    auto handle = dlopen("../lib/libomni_data_plane_udp_recver.so", RTLD_NOW | RTLD_GLOBAL);
     ASSERT_NE(handle, nullptr);
     dlclose(handle);
 }
 
-TEST(DataPlaneIpv4Sender, Create) {
+TEST(DataPlaneUdpRecver, Create) {
     using namespace omnistack::data_plane;
-    auto handle = dlopen("../lib/libomni_data_plane_ipv4_sender.so", RTLD_NOW | RTLD_GLOBAL);
+    auto handle = dlopen("../lib/libomni_data_plane_udp_recver.so", RTLD_NOW | RTLD_GLOBAL);
     ASSERT_NE(handle, nullptr);
-    auto ipv4_sender = ModuleFactory::instance_().Create(omnistack::common::ConstCrc32("Ipv4Sender"));
-    ASSERT_NE(ipv4_sender, nullptr);
+    auto udp_recver = ModuleFactory::instance_().Create(omnistack::common::ConstCrc32("UdpRecver"));
+    ASSERT_NE(udp_recver, nullptr);
     dlclose(handle);
 }
 
-TEST(DataPlaneIpv4Sender, Functions) {
+TEST(DataPlaneUdpRecver, Functions) {
     using namespace omnistack::data_plane;
     using namespace omnistack::packet;
     using namespace omnistack::node;
-    auto handle = dlopen("../lib/libomni_data_plane_ipv4_sender.so", RTLD_NOW | RTLD_GLOBAL);
+    auto handle = dlopen("../lib/libomni_data_plane_udp_recver.so", RTLD_NOW | RTLD_GLOBAL);
     ASSERT_NE(handle, nullptr);
-    auto ipv4_sender = ModuleFactory::instance_().Create(omnistack::common::ConstCrc32("Ipv4Sender"));
-    ASSERT_NE(ipv4_sender, nullptr);
-    Packet pack = Packet();
-    BasicNode node = BasicNode();
-    pack.node_ = &node;
-    node.info_.transport_layer_type = TransportLayerType::kTCP;
-    auto result = ipv4_sender->DefaultFilter(&pack);
-    //ASSERT_EQ(result, true);
-    //node.info_.transport_layer_type = TransportLayerType::kUDP;
-    //result = ipv4_sender->GetFilter(0, 0)(&pack);
-    //ASSERT_EQ(result, true);
-    result = ipv4_sender->allow_duplication_();
+    auto udp_recver = ModuleFactory::instance_().Create(omnistack::common::ConstCrc32("UdpRecver"));
+    ASSERT_NE(udp_recver, nullptr);
+    Packet packet_ipv4 = Packet();
+    Ipv4Header* ipv4_header = reinterpret_cast<Ipv4Header*>(packet_ipv4.data_ + packet_ipv4.offset_);
+    auto& ipv4 = packet_ipv4.packet_headers_[packet_ipv4.header_tail_ ++];
+    ipv4.length_ = 20;
+    ipv4.offset_ = packet_ipv4.offset_;
+    packet_ipv4.length_ = ntohs(ipv4_header->len) + packet_ipv4.offset_;
+    packet_ipv4.offset_ += ipv4.length_;
+    ipv4_header->version = 4;
+    ipv4_header->proto = IP_PROTO_TYPE_UDP;
+    auto result = udp_recver->DefaultFilter(&packet_ipv4);
     ASSERT_EQ(result, true);
-    auto type = ipv4_sender->type_();
+    Packet packet_ipv5 = Packet();
+    Ipv4Header* ipv5_header = reinterpret_cast<Ipv4Header*>(packet_ipv5.data_ + packet_ipv5.offset_);
+    auto& ipv5 = packet_ipv5.packet_headers_[packet_ipv5.header_tail_ ++];
+    ipv5.length_ = 20;
+    ipv5.offset_ = packet_ipv5.offset_;
+    packet_ipv5.length_ = ntohs(ipv5_header->len) + packet_ipv5.offset_;
+    packet_ipv5.offset_ += ipv5.length_;
+    ipv4_header->version = 5;
+    ipv4_header->proto = IP_PROTO_TYPE_UDP;
+    result = udp_recver->GetFilter(0, 0)(&packet_ipv5);
+    ASSERT_EQ(result, false);
+    result = udp_recver->allow_duplication_();
+    ASSERT_EQ(result, true);
+    auto type = udp_recver->type_();
     ASSERT_EQ(type, BaseModule::ModuleType::kReadWrite);
     dlclose(handle);
 }
