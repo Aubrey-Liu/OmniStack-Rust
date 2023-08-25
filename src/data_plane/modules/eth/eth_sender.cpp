91b6417b70132f4680b3baba457df8bcfdac273f
22040106-曾启桓 <2697541206@qq.com>
2023-08-25 17:09:13 +0800

    eth_sender: edit header finished without correct MAC address

---
+++ src/data_plane/modules/eth/eth_sender.cpp
@@ -0,0 +1,60 @@
+//
+// Created by zengqihuan on 2023-8-25
+//
+
+#include <omnistack/module/module.hpp>
+#include <omnistack/node.h>
+#include <omnistack/common/protocol_headers.hpp>
+
+namespace omnistack::data_plane::eth_sender {
+
+    using namespace omnistack::common;
+    using namespace omnistack::packet;
+    using namespace omnistack::node;
+
+    inline constexpr char kName[] = "EthSender";
+
+    class EthSender : public Module<EthSender, kName> {
+    public:
+        EthSender() {}
+
+        static bool DefaultFilter(Packet* packet);
+
+        Filter GetFilter(uint32_t upstream_node, uint32_t global_id) override { return DefaultFilter; };
+
+        Packet* MainLogic(Packet* packet) override;
+
+        constexpr bool allow_duplication_() override { return true; }
+
+        constexpr ModuleType type_() override { return ModuleType::kReadWrite; }
+    };
+
+    bool EthSender::DefaultFilter(Packet* packet) {
+        // confirm header protocol is ipv4/ipv6
+        auto proto = (*packet->node_).info_.network_layer_type;
+        return (proto == NetworkLayerType::kIPv4) || (proto == NetworkLayerType::kIPv6);
+    }
+
+    Packet* EthSender::MainLogic(Packet* packet) {
+        // edit omnistack's header
+        auto& eth = packet->packet_headers_[packet->header_tail_ ++];
+        eth.length_ = sizeof(EthernetHeader);
+        eth.offset_ = packet->offset_ - sizeof(EthernetHeader);
+        EthernetHeader* eth_header = reinterpret_cast<EthernetHeader*>(packet->data_ + eth.offset_);
+        packet->offset_ -= eth.length_;
+        packet->length_ += eth.length_;
+
+        // edit eth header
+        // TODO: set the MAC address according to packet->nic_
+        memset(eth_header->dst, packet->nic_, sizeof(eth_header->dst));
+        memset(eth_header->src, packet->nic_, sizeof(eth_header->src));
+        if((*packet->node_).info_.network_layer_type == NetworkLayerType::kIPv4)
+            eth_header->type = ETH_PROTO_TYPE_IPV4;
+        else if((*packet->node_).info_.network_layer_type == NetworkLayerType::kIPv6)
+            eth_header->type = ETH_PROTO_TYPE_IPV6;
+        else
+            eth_header->type = ETH_PROTO_TYPE_ARP;
+        return packet;
+    }
+
+}
