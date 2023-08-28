f8109479fb2f8fdea9aa6dce74c06f7c53f63c1f
JeremyGuo <jeremyguo@pku.edu.cn>
2023-08-28 20:25:43 +0800

    Finish test tools

--- tests/app/perf_test.cpp
+++ tests/app/perf_test.cpp
@@ -10,6 +10,7 @@
 
 #include <string>
 #include <arpa/inet.h>
+#include <sys/time.h>
 
 using namespace omnistack;
 
@@ -25,10 +26,17 @@ void InitOmniStack() {
     node::InitializeSubsystem();
 }
 
+static uint64_t GetCurrentTickUs() {
+    struct timeval tv;
+    gettimeofday(&tv, nullptr);
+    return tv.tv_sec * 1000000ll + tv.tv_usec;
+}
+
 namespace args {
     static std::string client_server_host = "";
     static std::string server_server_host = "";
     static int server_port;
+    static int size;
 
     bool is_reversed = false;
     bool is_pingpong = false;
@@ -45,6 +53,7 @@ int main(int argc, char **argv) {
     app.add_flag("--udp", args::is_udp, "UDP test")->default_val(false);
     app.add_flag("--reversed", args::is_reversed, "Reversed test")->default_val(false);
     app.add_flag("--pingpong", args::is_pingpong, "Pingpong test")->default_val(false);
+    app.add_option("--size", args::size, "The size per message")->default_val(1430);
 
     CLI11_PARSE(app, argc, argv);
     OMNI_LOG(common::kInfo) << "Arguments parsed.\n";
@@ -67,7 +76,11 @@ int main(int argc, char **argv) {
         server_addr.sin_port = htons(args::server_port);
         server_addr.sin_addr.s_addr = inet_addr(args::server_server_host.c_str());
         if (socket::bsd::bind(socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-            OMNI_LOG(common::kError) << "Failed to bind socket.\n";
+            OMNI_LOG(common::kError) << "Failed to bind socket." << std::endl;
+            return 1;
+        }
+        if (args::size < sizeof(uint64_t)) {
+            OMNI_LOG(common::kError) << "Size is too small." << std::endl;
             return 1;
         }
         OMNI_LOG(common::kInfo) << "Socket binded.\n";
@@ -75,7 +88,6 @@ int main(int argc, char **argv) {
             if (args::is_pingpong) {
                 if (!args::is_reversed) {
                     while (true) {
-                        char buffer[1024];
                         struct sockaddr_in client_addr;
                         socklen_t client_addr_len = sizeof(client_addr);
                         packet::Packet* packet;
@@ -84,9 +96,10 @@ int main(int argc, char **argv) {
                             OMNI_LOG(common::kError) << "Failed to recvfrom.\n";
                             return 1;
                         }
+
                         auto new_packet = socket::fast::write_begin(socket);
-                        memcpy(new_packet->data_.Get(), packet->data_ + packet->offset_, packet->length_ - packet->offset_);
-                        new_packet->length_ = packet->length_ - packet->offset_;
+                        memcpy(new_packet->data_.Get(), packet->data_ + packet->offset_, sizeof(uint64_t));
+                        new_packet->length_ = args::size;
                         socket::fast::read_over(socket, packet);
 
                         int send_len = socket::fast::sendto(socket, new_packet, 0, (struct sockaddr *)&client_addr, client_addr_len);
@@ -96,7 +109,6 @@ int main(int argc, char **argv) {
                         }
                     }
                 } else {
-                    char buffer[1024];
                     struct sockaddr_in client_addr;
                     socklen_t client_addr_len = sizeof(client_addr);
                     packet::Packet* packet;
@@ -105,11 +117,18 @@ int main(int argc, char **argv) {
                         OMNI_LOG(common::kError) << "Failed to recvfrom." << std::endl;
                         return 1;
                     }
+                    socket::fast::read_over(socket, packet);
+                    // Client Tell Address
+
+                    uint64_t last_print_tick = GetCurrentTickUs();
+                    uint64_t last_sum_tick = 0;
+                    int last_sum_tick_count = 0;
+
                     while (true) {
                         auto new_packet = socket::fast::write_begin(socket);
-                        memcpy(new_packet->data_.Get(), packet->data_ + packet->offset_, packet->length_ - packet->offset_);
-                        new_packet->length_ = packet->length_ - packet->offset_;
-                        socket::fast::read_over(socket, packet);
+                        auto cur_tick = GetCurrentTickUs();
+                        memcpy(new_packet->data_.Get(), &cur_tick, sizeof(cur_tick));
+                        new_packet->length_ = args::size;
                         int send_len = socket::fast::sendto(socket, new_packet, 0, (struct sockaddr *)&client_addr, client_addr_len);
                         if (send_len < 0) {
                             OMNI_LOG(common::kError) << "Failed to sendto.\n";
@@ -119,6 +138,153 @@ int main(int argc, char **argv) {
                         recv_len = socket::fast::recvfrom(socket, &packet, 0, (struct sockaddr *)&client_addr, &client_addr_len);
                         if (recv_len < 0) {
                             OMNI_LOG(common::kError) << "Failed to recvfrom.\n";
+                            return 1;
+                        }
+                        uint64_t end_tick = *reinterpret_cast<uint64_t*>(packet->data_ + packet->offset_);
+                        last_sum_tick = end_tick - cur_tick;
+                        last_sum_tick_count ++;
+                        socket::fast::read_over(socket, packet);
+
+                        if (end_tick - last_print_tick > 1000000) { // Print Per 1s
+                            OMNI_LOG(common::kInfo) << "Average RTT in last 1 second is " << last_sum_tick / last_sum_tick_count << "us." << std::endl;
+                            last_print_tick = end_tick;
+                            last_sum_tick = 0;
+                            last_sum_tick_count = 0;
+                        }
+                    }
+                }
+            } else {
+                if (!args::is_reversed) {
+                    /** Record the bandwidth of the last second **/
+                    uint64_t last_print_tick = GetCurrentTickUs();
+                    uint64_t last_sum_bytes = 0;
+
+                    while (true) {
+                        packet::Packet* packet;
+                        int recv_len = socket::fast::recvfrom(socket, &packet, 0, nullptr, nullptr);
+                        if (recv_len < 0) {
+                            OMNI_LOG(common::kError) << "Failed to recvfrom.\n";
+                            return 1;
+                        }
+                        last_sum_bytes += recv_len;
+                        auto current_tick = GetCurrentTickUs();
+                        if (current_tick - last_print_tick > 1000000) { // Print Per 1s
+                            OMNI_LOG(common::kInfo) << "Average bandwidth in last 1 second is " << 8.0 * last_sum_bytes / (current_tick - last_print_tick) << "Mbps." << std::endl;
+                            last_print_tick = current_tick;
+                            last_sum_bytes = 0;
+                        }
+                        socket::fast::read_over(socket, packet);
+                    }
+                } else {
+                    struct sockaddr_in client_addr;
+                    socklen_t client_addr_len = sizeof(client_addr);
+                    packet::Packet* packet;
+                    int recv_len = socket::fast::recvfrom(socket, &packet, 0, (struct sockaddr *)&client_addr, &client_addr_len);
+                    if (recv_len < 0) {
+                        OMNI_LOG(common::kError) << "Failed to recvfrom." << std::endl;
+                        return 1;
+                    }
+                    socket::fast::read_over(socket, packet);
+
+                    while (true) {
+                        auto new_packet = socket::fast::write_begin(socket);
+                        new_packet->length_ = args::size;
+                        int send_len = socket::fast::sendto(socket, new_packet, 0, (struct sockaddr *)&client_addr, client_addr_len);
+                        if (send_len < 0) {
+                            OMNI_LOG(common::kError) << "Failed to sendto.\n";
+                            return 1;
+                        }
+                    }
+                }
+            }
+        } else {
+            auto client = socket::bsd::accept(socket, nullptr, nullptr);
+            if (args::is_pingpong) {
+                if (!args::is_reversed) {
+                    while (true) {
+                        packet::Packet* packet;
+                        int recv_len = socket::fast::read(client, &packet);
+                        if (recv_len < 0) {
+                            OMNI_LOG(common::kError) << "Failed to recv.\n";
+                            return 1;
+                        }
+
+                        auto new_packet = socket::fast::write_begin(client);
+                        memcpy(new_packet->data_.Get(), packet->data_ + packet->offset_, sizeof(uint64_t));
+                        new_packet->length_ = args::size;
+                        socket::fast::read_over(client, packet);
+
+                        int send_len = socket::fast::write(client, new_packet);
+                        if (send_len < 0) {
+                            OMNI_LOG(common::kError) << "Failed to send.\n";
+                            return 1;
+                        }
+                    }
+                } else {
+                    uint64_t last_print_tick = GetCurrentTickUs();
+                    uint64_t last_sum_tick = 0;
+                    int last_sum_tick_count = 0;
+
+                    /* Send a packet to client with curernt tick and wait the packet back and calc
+                        the average latency and print per second. */
+                    while (true) {
+                        auto new_packet = socket::fast::write_begin(client);
+                        auto cur_tick = GetCurrentTickUs();
+                        memcpy(new_packet->data_.Get(), &cur_tick, sizeof(cur_tick));
+                        new_packet->length_ = args::size;
+                        int send_len = socket::fast::write(client, new_packet);
+                        if (send_len < 0) {
+                            OMNI_LOG(common::kError) << "Failed to send.\n";
+                            return 1;
+                        }
+
+                        packet::Packet* packet;
+                        int recv_len = socket::fast::read(client, &packet);
+                        if (recv_len < 0) {
+                            OMNI_LOG(common::kError) << "Failed to recv.\n";
+                            return 1;
+                        }
+                        uint64_t end_tick = *reinterpret_cast<uint64_t*>(packet->data_ + packet->offset_);
+                        last_sum_tick = end_tick - cur_tick;
+                        last_sum_tick_count ++;
+                        socket::fast::read_over(client, packet);
+
+                        if (end_tick - last_print_tick > 1000000) { // Print Per 1s
+                            OMNI_LOG(common::kInfo) << "Average RTT in last 1 second is " << last_sum_tick / last_sum_tick_count << "us." << std::endl;
+                            last_print_tick = end_tick;
+                            last_sum_tick = 0;
+                            last_sum_tick_count = 0;
+                        }
+                    }
+                }
+            } else {
+                if (!args::is_reversed) {
+                    uint64_t last_print_tick = GetCurrentTickUs();
+                    uint64_t last_sum_bytes = 0;
+
+                    while (true) {
+                        packet::Packet* packet;
+                        int recv_len = socket::fast::read(client, &packet);
+                        if (recv_len < 0) {
+                            OMNI_LOG(common::kError) << "Failed to recv.\n";
+                            return 1;
+                        }
+                        last_sum_bytes += recv_len;
+                        auto current_tick = GetCurrentTickUs();
+                        if (current_tick - last_print_tick > 1000000) { // Print Per 1s
+                            OMNI_LOG(common::kInfo) << "Average bandwidth in last 1 second is " << 8.0 * last_sum_bytes / (current_tick - last_print_tick) << "Mbps." << std::endl;
+                            last_print_tick = current_tick;
+                            last_sum_bytes = 0;
+                        }
+                        socket::fast::read_over(client, packet);
+                    }
+                } else {
+                    while (true) {
+                        auto new_packet = socket::fast::write_begin(client);
+                        new_packet->length_ = args::size;
+                        int send_len = socket::fast::write(client, new_packet);
+                        if (send_len < 0) {
+                            OMNI_LOG(common::kError) << "Failed to send.\n";
                             return 1;
                         }
                     }
